737f4103451efeb820e93d3d03b53bf173d422a0
Sabio <gamester.sad@gmail.com>
2025-12-26 20:00:32 +0000

    update website (now looks cool!)

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ export default function Home() {
           <source media="(prefers-color-scheme: dark)" srcSet="https://i.ibb.co/4R9q41hn/Group-11.png"></source>
           <img src={`https://i.ibb.co/yBhBsGyf/Group-11-1.png`} alt="Spicetown Logo" width={0} height={0} className="w-auto h-16"></img>
         </picture>
-        <a href="https://chromewebstore.google.com/detail/Spicetown/kbcbgiihiopcbgimdopcfcemikagemgl"><img src={`./chromewebstore-badge.png`} className="w-auto h-12"></img></a>
+        <div className="flex flex-row gap-8">
+          <a href="https://chromewebstore.google.com/detail/Spicetown/kbcbgiihiopcbgimdopcfcemikagemgl"><img src={`./chromewebstore-badge.png`} className="w-auto h-12"></img></a>
+          <a href="https://addons.mozilla.org/en-GB/firefox/addon/spicetown/"><img src={`./get-the-addon-fx-apr-2020.svg`} className="w-auto h-12"></img></a>
+        </div>
       </div>
       <div className="flex flex-row gap-16">
         <picture>
@@ -15,7 +18,7 @@ export default function Home() {
           <source media="(prefers-color-scheme: dark)" srcSet="https://i.ibb.co/TMXsSN8b/Group-16.png"></source>
           <img src={`https://i.ibb.co/B5vswjX3/Group-15.png`} alt="Spicetown Logo" width={0} height={0} className="w-auto h-16"></img>
         </picture>
-        <a href="https://github.com/SabioOfficial/spicetown/actions/runs/20503051614/artifacts/4966027997">
+        <a href="https://github.com/SabioOfficial/spicetown/actions/workflows/build-extension-zip.yml">
           <picture>
             <source media="(prefers-color-scheme: light)" srcSet="./github-mark.png"></source>
             <source media="(prefers-color-scheme: dark)" srcSet="./github-mark-white.png"></source>
